aad5103dff59e23d1637a9c012b3fd05d4cf81c6
Mohamud Ali Farah <mohamudalifarah48@gmail.com>
2023-01-27 19:27:49 +0000

    add first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='class_container'>
+        <span>Deployment with Terraform</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
